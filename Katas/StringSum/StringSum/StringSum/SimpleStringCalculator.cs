9e0fccab8b6c42d3ea321a25aaeb6119cbcdc37f
Anton Naidunov <naidunovanton@gmail.com>
2021-01-22 19:19:28 +0300

    Add SimpleStringCalculator and make all tests passed

--- Katas/StringSum/StringSum/StringSum/SimpleStringCalculator.cs
+++ Katas/StringSum/StringSum/StringSum/SimpleStringCalculator.cs
@@ -6,7 +6,35 @@ namespace StringSum
     {
         public string Sum(string numString1, string numString2)
         {
-            throw new NotImplementedException();
+            CheckArgs(numString1, numString2);
+
+            var num1 = double.Parse(numString1);
+            var num2 = double.Parse(numString2);
+
+            num1 = IsNatural(num1) ? num1 : 0;
+            num2 = IsNatural(num2) ? num2 : 0;
+
+            var sum = checked((long)num1 + (long)num2);
+
+            return sum.ToString();
+        }
+
+        private void CheckArgs(string num1, string num2)
+        {
+            if (string.IsNullOrEmpty(num1))
+            {
+                throw new ArgumentException(nameof(num1));
+            }
+
+            if (string.IsNullOrEmpty(num2))
+            {
+                throw new ArgumentException(nameof(num2));
+            }
+        }
+
+        private bool IsNatural(double num)
+        {
+            return Math.Abs(num % 1) < double.Epsilon * 100;
         }
     }
 }
